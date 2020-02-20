af7317510223e15427b64752c3254fc3390e77d9
Kreas41 <bugrademir1812@hotmail.com>
2020-02-20 21:39:37 +0300

    Added AuotonomousCommand

--- src/main/java/frc/robot/commands/AutonomousCommand.java
+++ src/main/java/frc/robot/commands/AutonomousCommand.java
@@ -14,22 +14,19 @@ public class AutonomousCommand extends CommandGroup {
    * Add your docs here.
    */
   public AutonomousCommand() {
-    
-    // Add Commands here:
-    // e.g. addSequential(new Command1());
-    // addSequential(new Command2());
-    // these will run in order.
-
-    // To run multiple commands at the same time,
-    // use addParallel()
-    // e.g. addParallel(new Command1());
-    // addSequential(new Command2());
-    // Command1 and Command2 will run in parallel.
-
-    // A command group will require all of the subsystems that each member
-    // would require.
-    // e.g. if Command1 requires chassis, and Command2 requires arm,
-    // a CommandGroup containing them would require both the chassis and the
-    // arm.
+    addSequential(new ShooterManuel(1),2);
+    addSequential(new HazneManuel(1),3);
+    addParallel(new ShooterManuel(1));
+    addSequential(new GyroTurn(45));
+    addParallel(new GyroDrive(true),2);
+    addSequential(new GyroTurn(0));
+    addParallel(new GyroDrive(true),2);
+    addParallel(new Intake(-1));
+    addParallel(new HazneManuel(1),2);
+    addSequential(new GyroTurn(45));
+    addParallel(new GyroDrive(false),2);
+    addSequential(new GyroTurn(0));
+    addParallel(new GyroDrive(false),2);
+ 
   }
 }
